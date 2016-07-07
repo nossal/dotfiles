4d25786aa09d0c297ebb0b319294f5838f3bc0ea
Rodrigo Nossal <rodrigo@noss.al>
2016-07-06 23:11:36 -0300

    ../..

--- gnome/defaults.zsh
+++ gnome/defaults.zsh
@@ -2,5 +2,5 @@
 
 # gnome GTK3 window button-layout on the left (Mac style)
 exec gsettings set org.gnome.desktop.wm.preferences button-layout 'close:';
-# same above for GTK2 apps (ie: google chrome)
+# same as the above for GTK2 apps (ie: google chrome)
 exec gconftool-2 --set /apps/metacity/general/button_layout --type string "close,maximize,minimize:";
