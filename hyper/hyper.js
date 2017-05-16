c2e1905ae9eb202551eb27f11895b0043c636196
Rodrigo Nossal <nossal@not38227.corp.terra>
2017-05-16 16:50:39 -0300

    update; hyper.js

---
+++ hyper/hyper.js
@@ -0,0 +1,119 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
+module.exports = {
+  config: {
+    // default font size in pixels for all tabs
+    fontSize: 12,
+
+    // font family with optional fallbacks
+    fontFamily: 'Meslo LG S for Powerline Bold, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+
+    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    cursorColor: 'rgba(200,60,60,0.8)',
+
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    cursorShape: 'BLOCK',
+
+    // set to true for blinking cursor
+    cursorBlink: true,
+
+    // color of the text
+    foregroundColor: '#a8a9b0',
+
+    // terminal background color
+    backgroundColor: '#282b35',
+
+    // border color (window, tabs)
+    borderColor: '#333',
+
+    // custom css to embed in the main window
+    css: '',
+
+    // custom css to embed in the terminal window
+    termCSS: '',
+
+    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
+    // default: `false` on Linux, `true` on Windows (ignored on macOS)
+    showHamburgerMenu: true,
+
+    // set to `false` if you want to hide the minimize, maximize and close buttons
+    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
+    // default: `true` on windows and Linux (ignored on macOS)
+    showWindowControls: 'left',
+
+    // custom padding (css format, i.e.: `top right bottom left`)
+    padding: '2px 4px',
+
+    // the full list. if you're going to provide the full color palette,
+    // including the 6 x 6 color cubes and the grayscale map, just provide
+    // an array here instead of a color map object
+    colors: {
+      black: '#282b35',
+      red: '#dc3c3c',
+      green: '#4eb355',
+      yellow: '#d28e5d',
+      blue: '#3e758c',
+      magenta: '#b73999',
+      cyan: '#00aba5',
+      white: '#a8a9b0',
+      lightBlack: '#393c45',
+      lightRed: '#db5151',
+      lightGreen: '#95c76f',
+      lightYellow: '#d99d72',
+      lightBlue: '#5694ad',
+      lightMagenta: '#c85eaf',
+      lightCyan: '#24bdb7',
+      lightWhite: '#c7c9cf'
+    },
+
+    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+    // if left empty, your system's login shell will be used by default
+    // make sure to use a full path if the binary name doesn't work
+    // (e.g `C:\\Windows\\System32\\bash.exe` instead of just `bash.exe`)
+    // if you're using powershell, make sure to remove the `--login` below
+    shell: '',
+
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
+    shellArgs: ['--login'],
+
+    // for environment variables
+    env: {},
+
+    // set to false for no bell
+    bell: 'SOUND',
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: true,
+
+    // if true, on right click selected text will be copied or pasted if no
+    // selection is present (true by default on Windows)
+    // quickEdit: true
+
+    // URL to custom bell
+    // bellSoundURL: 'http://example.com/bell.mp3',
+
+    // for advanced config flags please refer to https://hyper.is/#cfg
+    hyperBorder: {
+      // animate: true,
+      borderColors: ['#fc1da7', '#fba506'],
+      borderAngle: '180deg',
+      borderWidth: '2px'
+    }
+  },
+
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  // examples:
+  //   `hyperpower`
+  //   `@company/project`
+  //   `project#1.0.1`
+  plugins: ['hyperborder', 'hyper-mac-controls'],
+
+  // in development, you can create a directory under
+  // `~/.hyper_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: []
+};
