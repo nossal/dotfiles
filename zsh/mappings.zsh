191bc177a7665d0a7fb0dc53d0fd946ed7c805dd
Rodrigo Nossal <rodrigo@noss.al>
2023-11-04 22:44:38 -0300

    fix zsh

--- zsh/mappings.zsh
+++ zsh/mappings.zsh
@@ -8,3 +8,4 @@ alias la="ll -a"
 alias lt="ls --tree"
 
 alias xclip="xclip -selection clipboard"
+alias v="fd --type f --hidden --exclude 'node_modules .m2 .git' | fzf-tmux -p --reverse | xargs nvim"
