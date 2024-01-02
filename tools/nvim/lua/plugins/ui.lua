ff68ad1209e9dfb9964ce25dfb6c9359c690ffa3
Rodrigo Nossal <rodrigo@noss.al>
2024-01-02 12:36:44 -0300

    nvim: notify

--- tools/nvim/lua/plugins/ui.lua
+++ tools/nvim/lua/plugins/ui.lua
@@ -1,4 +1,17 @@
 return {
-  'stevearc/dressing.nvim',
-  opts = {},
+	{
+		"stevearc/dressing.nvim",
+		opts = {},
+	},
+	{
+		"rcarriga/nvim-notify",
+		config = function()
+			local notify = require("notify")
+			notify.setup({
+				render = "compact",
+				stages = "fade",
+			})
+			vim.notify = notify
+		end,
+	},
 }
