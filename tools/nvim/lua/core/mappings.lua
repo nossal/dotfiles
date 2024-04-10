9c638891ac10c88fe6f233b2def923e622cf3a66
Rodrigo Nossal <rodrigo@noss.al>
2024-04-10 19:57:19 -0300

    neovim: update global keymappings

--- tools/nvim/lua/core/mappings.lua
+++ tools/nvim/lua/core/mappings.lua
@@ -3,9 +3,12 @@ vim.g.maplocalleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>L", ":Lazy<CR>", { desc = "Lazy neovim plugin manager" })
+keymap.set("n", "<leader>M", ":Mason<CR>", { desc = "Mason LSP package manager" })
 
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>ch", ":nohl<CR>", { desc = "Clear search highlights" })
+
+keymap.set("n", "<lcader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
