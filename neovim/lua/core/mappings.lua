5a611a36a34b9f8be8f480eab6ad53f896f5a595
Rodrigo Nossal <rodrigo@noss.al>
2024-05-15 02:35:14 -0300

    neovim: fix typo mapping

--- neovim/lua/core/mappings.lua
+++ neovim/lua/core/mappings.lua
@@ -8,7 +8,7 @@ keymap.set("n", "<leader>M", ":Mason<CR>", { desc = "Mason LSP package manager" 
 
 keymap.set("n", "<leader>ch", ":nohl<CR>", { desc = "Clear search highlights" })
 
-keymap.set("n", "<lcader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
