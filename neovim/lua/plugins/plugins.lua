c00b49faa27de83a57f41f4cbfa7b970a2a058c0
Rodrigo Nossal <rodrigo@noss.al>
2024-05-15 23:50:56 -0300

    neovim/tmux navigation

--- neovim/lua/plugins/plugins.lua
+++ neovim/lua/plugins/plugins.lua
@@ -1,13 +1,30 @@
 return {
   {
     "chipsenkbeil/distant.nvim",
-    cmd = { "DistantConnect", "DistantLaunch", "DistantInstall", "DistantOpen"},
+    cmd = { "DistantConnect", "DistantLaunch", "DistantInstall", "DistantOpen" },
     branch = "v0.3",
     config = function()
       require("distant"):setup({
         ["network.private"] = true,
         ["network.unix_socket"] = "/tmp/distant.sock",
       })
+    end,
+  },
+  {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+      nvim_tmux_nav.setup({
+        disable_when_zoomed = true, -- defaults to false
+      })
+
+      vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+      vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+      vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+      -- vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+      vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+      -- vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
     end,
   },
   --[[ "smoka7/hop.nvim",
