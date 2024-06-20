29c4063432f2af4c1ad60bf911208155f4398c0a
nossal <rodrigo@noss.al>
2024-06-19 23:33:55 -0300

    neovim: lsp sonarlint

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -6,6 +6,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
 
+    "danarth/sonarlint.nvim",
     -- Additional lua configuration, makes nvim stuff amazing!
     { "folke/neodev.nvim", opts = {} },
   },
@@ -101,5 +102,37 @@ return {
         },
       })
     end
+
+    require("sonarlint").setup({
+      server = {
+        cmd = {
+          "sonarlint-language-server",
+          -- Ensure that sonarlint-language-server uses stdio channel
+          "-stdio",
+          "-analyzers",
+          -- paths to the analyzers you need, using those for python and java in this example
+          vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
+          vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+        },
+        -- All settings are optional
+        settings = {
+          -- The default for sonarlint is {}, this is just an example
+          sonarlint = {
+            rules = {
+              ["typescript:S101"] = { level = "on", parameters = { format = "^[A-Z][a-zA-Z0-9]*$" } },
+              ["typescript:S103"] = { level = "on", parameters = { maximumLineLength = 180 } },
+              ["typescript:S106"] = { level = "on" },
+              ["typescript:S107"] = { level = "on", parameters = { maximumFunctionParameters = 7 } },
+            },
+          },
+        },
+      },
+      filetypes = {
+        -- Tested and working
+        "python",
+        -- Requires nvim-jdtls, otherwise an error message will be printed
+        "java",
+      },
+    })
   end,
 }
