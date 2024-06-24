f6d554af731acb93f3238343e338a210d06c477f
nossal <rodrigo@noss.al>
2024-06-24 02:00:37 -0300

    neovim: noice ui changes

--- neovim/lua/plugins/ui.lua
+++ neovim/lua/plugins/ui.lua
@@ -35,7 +35,36 @@ return {
         },
         hover = {
           enabled = false,
+        },
+      },
+      cmdline = {
+        format = {
+          cmdline = { pattern = "^:", icon = " ", lang = "vim" },
+          search_down = { kind = "search", pattern = "^/", icon = "  ", lang = "regex" },
+          search_up = { kind = "search", pattern = "^%?", icon = "  ", lang = "regex" },
+          filter = { pattern = "^:%s*!", icon = " ", lang = "bash" },
+          lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = " ", lang = "lua" },
+          help = { pattern = "^:%s*he?l?p?%s+", icon = "󰋖 " },
+          input = { view = "cmdline_input", icon = "󰥻 " }, -- Used by input()
         }
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = 5,
+            col = "50%",
+          },
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "written",
+          },
+          opts = { skip = true },
+        },
       },
       -- add any options here
     },
