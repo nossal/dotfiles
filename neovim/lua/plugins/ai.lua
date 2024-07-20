e66524897dbbdf3eb3db8978081af0981f2bf242
nossal <rodrigo@noss.al>
2024-07-20 16:30:45 -0300

    neovim: ai update

--- neovim/lua/plugins/ai.lua
+++ neovim/lua/plugins/ai.lua
@@ -1,20 +1,31 @@
+
 return {
   "huggingface/llm.nvim",
   enabled = false,
   opts = {
     backend = "ollama",
-    url = "http://192.168.1.236:11434",
-    tokens_to_clear = { "<EOT>" },
+    url = "http://ai.local:11434",
+    tokens_to_clear = { "<|file_separator|>" },
     fim = {
       enabled = true,
-      prefix = "<PRE> ",
-      middle = " <MID>",
-      suffix = " <SUF>",
+      prefix = "<|fim_prefix|> ",
+      middle = " <|fim_middle|>",
+      suffix = " <|fim_suffix|>",
     },
-    model = "codellama:7b-code",
-    context_window = 4096,
+    request_body = {
+      options = {
+        num_predict = 128,
+        temperature =  0,
+        top_p =  0.9,
+      },
+    },
+    model = "codegemma:2b-code",
+    -- model = "llama3",
+    -- model = "codellama:7b-code",
+    -- context_window = 4096,
     tokenizer = {
-      repository = "codellama/CodeLlama-7b-hf",
+      repository = "google/codegemma-1.1-2b"
+    --   repository = "codellama/CodeLlama-7b-hf",
     },
   },
 }
