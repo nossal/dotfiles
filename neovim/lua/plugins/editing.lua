1f0cf471bc254fba978ec20337bca00fd23f655f
nossal <rodrigo@noss.al>
2024-10-21 13:55:33 -0300

    neovim: no comments

--- neovim/lua/plugins/editing.lua
+++ neovim/lua/plugins/editing.lua
@@ -43,26 +43,6 @@ return {
     end,
   },
   {
-    "numToStr/Comment.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-    },
-    config = function()
-      -- import comment plugin safely
-      local comment = require("Comment")
-      -- comment.setup()
-
-      local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-      -- enable comment
-      comment.setup({
-        -- for commenting tsx and jsx files
-        pre_hook = ts_context_commentstring.create_pre_hook(),
-      })
-    end,
-  },
-  {
     "RRethy/vim-illuminate",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
