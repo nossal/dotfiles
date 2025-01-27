dfce1f02db51c60df95543f9b00acd709637c10d
nossal <rodrigo@noss.al>
2025-01-27 12:17:56 -0300

    neovim: clean some plugins

--- neovim/lua/plugins/plugins.lua
+++ neovim/lua/plugins/plugins.lua
@@ -1,6 +1,7 @@
 return {
   {
     "chipsenkbeil/distant.nvim",
+    enabled = false,
     cmd = { "DistantConnect", "DistantLaunch", "DistantInstall", "DistantOpen" },
     branch = "v0.3",
     config = function()
@@ -27,24 +28,8 @@ return {
       vim.keymap.set("n", "<M-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
     end,
   },
-
-  --[[ "smoka7/hop.nvim",
-	version = "*",
-	config = function()
-		-- place this in one of your configuration file(s)
-		local hop = require("hop")
-		local directions = require("hop.hint").HintDirection
-		vim.keymap.set("", "f", function()
-			hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-		end, { remap = true })
-		vim.keymap.set("", "F", function()
-			hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-		end, { remap = true })
-		vim.keymap.set("", "t", function()
-			hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-		end, { remap = true })
-		vim.keymap.set("", "T", function()
-			hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-		end, { remap = true })
-	end, ]]
+  {
+    "eandrju/cellular-automaton.nvim",
+    enabled = false,
+  },
 }
