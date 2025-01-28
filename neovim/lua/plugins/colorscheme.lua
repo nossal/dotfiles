7f189913f9b47e344e4e84e63a3652f4618f52f8
nossal <rodrigo@noss.al>
2025-01-28 15:10:41 -0300

    neovim: coloscheme tweek

--- neovim/lua/plugins/colorscheme.lua
+++ neovim/lua/plugins/colorscheme.lua
@@ -6,17 +6,33 @@ return {
   },
   {
     "craftzdog/solarized-osaka.nvim",
+    -- enabled = false,
     lazy = false,
     priority = 1000,
     opts = {},
     config = function()
+      local util = require("solarized-osaka.util")
       require("solarized-osaka").setup({
-        transparent = true,
+        transparent = false,
         styles = {
           dim_inactive = true,
+          floats = "dark",
         },
         on_colors = function(colors)
           colors.green = "#0aaf27"
+        end,
+        on_highlights = function(hl, c)
+          hl.CursorLine = {
+            -- bg = util.darken(c.bg, 0.1),
+            bg = util.lighten(c.bg, 0.97),
+          }
+          hl.BlinkCmpMenu = {
+            bg = util.lighten(c.bg, 0.95),
+            fg = c.fg,
+          }
+          hl.BlinkCmpMenuSelection = {
+            bg = util.lighten(c.bg, 0.85),
+          }
         end,
       })
       vim.cmd([[colorscheme solarized-osaka]])
