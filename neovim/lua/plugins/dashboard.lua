c1b84cab8d1916676ec831c0e9d84612d63c718f
nossal <rodrigo@noss.al>
2025-02-08 16:27:18 -0300

    neovim: a dashboard

---
+++ neovim/lua/plugins/dashboard.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    dashboard = {
+      -- your dashboard configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
+  },
+}
