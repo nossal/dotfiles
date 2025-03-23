2a210456895a3a63b0cca1393546bb1cd19ff5c0
nossal <rodrigo@noss.al>
2025-03-23 03:29:28 -0300

    neovim: new dashboard header

--- neovim/lua/plugins/ui.lua
+++ neovim/lua/plugins/ui.lua
@@ -13,14 +13,11 @@ return {
         },
       },
       dashboard = {
-        -- your dashboard configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
         sections = {
-          { section = "header" },
+          { section = "terminal", cmd = "cat ~/.dotfiles/neovim/neo.txt", height = 8 , padding = 1 },
           { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
           { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          -- { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
           { section = "startup" },
         },
       },
