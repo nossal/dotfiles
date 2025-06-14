e4d9edcb7d9ddefd3f98c5133931eee8f3f88164
Nossal, Rodrigo <rodrigo.nossal@telefonica.com>
2025-06-13 21:41:02 -0300

    shell update

--- shell/zsh/functions.zsh
+++ shell/zsh/functions.zsh
@@ -1,48 +1,12 @@
-# Function to source files if they exist
-function source_file() {
-  [ -f "$ZSHDOTDIR/$1" ] && source "$ZSHDOTDIR/$1"
-}
-
-function load_plugins() {
-  for plugin in $@; do
-    plugin_name=$(echo $plugin | cut -d "/" -f 2)
-
-    if [ ! -d "$ZSHDOTDIR/plugins/$plugin_name" ]; then
-      git clone "https://github.com/$plugin.git" "$ZSHDOTDIR/plugins/$plugin_name"
-    fi
-
-    # echo "$plugin_name"
-    source_file "plugins/$plugin_name/$plugin_name.plugin.zsh" ||
-      source_file "plugins/$plugin_name/$plugin_name.zsh"
-  done
-}
-
-function zsh_add_completion() {
-  plugin_name=$(echo $1 | cut -d "/" -f 2)
-  if [ -d "$ZSHDOTDIR/completions/$plugin_name" ]; then
-    # For completions
-    completion_file_path=$(ls $ZSHDOTDIR/completions/$plugin_name/*/_*)
-    fpath+="$(dirname "${completion_file_path}")"
-    source_file "completions/$plugin_name/$plugin_name.plugin.zsh"
-  else
-    git clone "https://github.com/$1.git" "$ZSHDOTDIR/completions/$plugin_name"
-    fpath+=$(ls $ZSHDOTDIR/completions/$plugin_name/*/_*)
-    [ -f $ZSHDOTDIR/.zccompdump ] && $ZSHDOTDIR/.zccompdump
-  fi
-  completion_file="$(basename "${completion_file_path}")"
-  [ "$2" = true ] && compinit "${completion_file:1}"
-}
-
 _priv_func() {
   echo "Private Function"
 }
 
 function ssht() {
-  /usr/bin/ssh -t "$@" "tmux attach || tmux new"
+  ssh -t "$1" "/usr/local/bin/tmux new -A -s $2"
 }
 
 function hostname() {
-
   echo $(cat /proc/sys/kernel/hostname)
 }
 
---
+++ shell/zsh/plug.zsh
@@ -0,0 +1,35 @@
+# Function to source files if they exist
+function source_file() {
+  [ -f "$ZSHDOTDIR/$1" ] && source "$ZSHDOTDIR/$1"
+}
+
+function load_plugins() {
+  for plugin in $@; do
+    plugin_name=$(echo $plugin | cut -d "/" -f 2)
+
+    if [ ! -d "$ZSHDOTDIR/plugins/$plugin_name" ]; then
+      git clone "https://github.com/$plugin.git" "$ZSHDOTDIR/plugins/$plugin_name"
+    fi
+
+    # echo "$plugin_name"
+    source_file "plugins/$plugin_name/$plugin_name.plugin.zsh" ||
+      source_file "plugins/$plugin_name/$plugin_name.zsh"
+  done
+}
+
+function zsh_add_completion() {
+  plugin_name=$(echo $1 | cut -d "/" -f 2)
+  if [ -d "$ZSHDOTDIR/completions/$plugin_name" ]; then
+    # For completions
+    completion_file_path=$(ls $ZSHDOTDIR/completions/$plugin_name/*/_*)
+    fpath+="$(dirname "${completion_file_path}")"
+    source_file "completions/$plugin_name/$plugin_name.plugin.zsh"
+  else
+    git clone "https://github.com/$1.git" "$ZSHDOTDIR/completions/$plugin_name"
+    fpath+=$(ls $ZSHDOTDIR/completions/$plugin_name/*/_*)
+    [ -f $ZSHDOTDIR/.zccompdump ] && $ZSHDOTDIR/.zccompdump
+  fi
+  completion_file="$(basename "${completion_file_path}")"
+  [ "$2" = true ] && compinit "${completion_file:1}"
+}
+
