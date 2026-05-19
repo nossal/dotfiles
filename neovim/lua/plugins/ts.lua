fe341339818001fc1c005d52f6333e05b2dc4872
nossal <rodrigo@noss.al>
2026-05-19 15:58:09 -0300

    neovim: ts initializaton

--- neovim/lua/plugins/ts.lua
+++ neovim/lua/plugins/ts.lua
@@ -7,7 +7,7 @@ return {
   },
   {
     "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
+    lazy = false,
     build = ":TSUpdate",
     opts = {
       ensure_installed = {
