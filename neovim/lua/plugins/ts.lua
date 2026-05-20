4c0248714121e7ab74d4fbdba74809b01e0a60b5
nossal <rodrigo@noss.al>
2026-05-19 23:09:24 -0300

    neovim: new tree-sitter setup

--- neovim/lua/plugins/ts.lua
+++ neovim/lua/plugins/ts.lua
@@ -1,16 +1,17 @@
 return {
   {
-   "nvim-mini/mini.ai",
+    "nvim-mini/mini.ai",
     config = function()
       require("mini.ai").setup()
     end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     lazy = false,
     build = ":TSUpdate",
-    opts = {
-      ensure_installed = {
+    init = function()
+      local ensureInstalled = {
         "bash",
         "html",
         "java",
@@ -31,7 +32,24 @@ return {
         "gitcommit",
         "diff",
         "git_rebase",
-      },
-    },
+      }
+      local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+      local parsersToInstall = vim
+        .iter(ensureInstalled)
+        :filter(function(parser)
+          return not vim.tbl_contains(alreadyInstalled, parser)
+        end)
+        :totable()
+      require("nvim-treesitter").install(parsersToInstall)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          -- Enable treesitter highlighting and disable regex syntax
+          pcall(vim.treesitter.start)
+          -- Enable treesitter-based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
   },
 }
