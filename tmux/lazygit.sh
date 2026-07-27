f9d03e2e273119b0ecdd999a0a4d8c38a4c5e3cd
nossal <rodrigo@noss.al>
2026-07-27 18:36:45 -0300

    fix lazygit script
    lazygit script handle calls inside the repo_dir

--- tmux/lazygit.sh
+++ tmux/lazygit.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
-eval "$HOME/.local/share/mise/shims/lazygit -p $PWD"
+repo_dir="$(git rev-parse --show-toplevel)"
+eval "$HOME/.local/share/mise/shims/lazygit -p $repo_dir"
+
